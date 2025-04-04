249cd7c20c7ea71446752287c3c2082f4dabb508
littleV <wei.vera.zhang@gmail.com>
2025-04-04 21:52:26 +0800

    configure github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "", // Only needed if not using a custom domain
+  images: {
+    unoptimized: true, // For static export
+  },
+};
 
 export default nextConfig;
